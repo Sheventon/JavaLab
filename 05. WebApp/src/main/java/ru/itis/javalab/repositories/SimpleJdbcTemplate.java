4e2a798a76f386da7c84a2a4bd6bfcbb2899b4da
Sheventon <tores.fernando.real@gmail.com>
2020-10-15 13:56:52 +0300

    05. SimpleJdbcTemplate

---
+++ 05. WebApp/src/main/java/ru/itis/javalab/repositories/SimpleJdbcTemplate.java
@@ -0,0 +1,72 @@
+package ru.itis.javalab.repositories;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleJdbcTemplate {
+
+    private final DataSource dataSource;
+
+    public SimpleJdbcTemplate(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        System.out.println(args.length);
+
+        try {
+            connection = dataSource.getConnection();
+            statement = connection.prepareStatement(sql);
+
+            int position = 1;
+            for(Object o : args) {
+                statement.setObject(position, o);
+                position++;
+            }
+            resultSet = statement.executeQuery();
+
+            List<T> objects = new ArrayList<>();
+
+            if (resultSet == null) {
+                System.out.println("Empty list");
+            }
+
+            while (resultSet.next()) {
+                objects.add(rowMapper.mapRow(resultSet));
+            }
+
+            return objects;
+
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException ignore) {
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException ignore) {
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException ignore) {
+                }
+            }
+        }
+    }
+}
+
---
+++ 05. WebApp/src/main/java/ru/itis/javalab/repositories/UsersRepositoryJdbcImpl.java
@@ -0,0 +1,79 @@
+package ru.itis.javalab.repositories;
+
+import ru.itis.javalab.models.User;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Optional;
+
+public class UsersRepositoryJdbcImpl implements UsersRepository {
+
+    //language=SQL
+    private static final String SQL_SELECT_BY_AGE = "select * from users where age = ?";
+
+    //language=SQL
+    private static final String SQL_SELECT_BY_AGE_AND_LASTNAME = "select * from users where age = ? and lastname = ?";
+
+    //language=SQL
+    private static final String SQL_SELECT = "select * from users";
+
+    private DataSource dataSource;
+
+    private SimpleJdbcTemplate template;
+
+    public UsersRepositoryJdbcImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+        this.template = new SimpleJdbcTemplate(dataSource);
+    }
+
+    private RowMapper<User> userRowMapper = row -> User.builder()
+            .id(row.getLong("id"))
+            .firstName(row.getString("firstname"))
+            .lastName(row.getString("lastname"))
+            .age(row.getInt("age"))
+            .build();
+
+    public List<User> findByAgeAndLastname(Integer age, String lastname) {
+        return template.query(SQL_SELECT_BY_AGE_AND_LASTNAME, userRowMapper, age, lastname);
+    }
+
+    @Override
+    public List<User> findAllByAge(Integer age) {
+        return template.query(SQL_SELECT_BY_AGE, userRowMapper, age);
+    }
+
+    @Override
+    public Optional<User> findFirstByFirstnameAndLastname(String firstName, String lastName) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return template.query(SQL_SELECT, userRowMapper);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void save(User entity) {
+
+    }
+
+    @Override
+    public void update(User entity) {
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+    }
+
+    @Override
+    public void delete(User entity) {
+
+    }
+}
